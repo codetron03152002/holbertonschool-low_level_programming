304f1c7de6c8f7fcb5670f9dfa70114a6d865c46
codetron03152002 <codetron03152002>
2023-02-17 19:04:00 +0000

    comment

---
+++ hello_world/4-puts.c
@@ -0,0 +1,6 @@
+#!/bin/bash
+gcc -v Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+
+    Use the function puts
+    You are not allowed to use printf
+    Your program should end with the value 0 
