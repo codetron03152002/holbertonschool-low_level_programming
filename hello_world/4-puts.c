58f13ce7d8c00002ef1c7edbfc3a68481dc3ee71
codetron03152002 <codetron03152002>
2023-02-19 03:30:18 +0000

    comment

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- *
- * main -A program that prints a line whith puts function
- * Return: 0 (Success)
- */
-int main(void
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+* main - entry point 
+* * Return: program should end with value 0
+*/
 
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0):
+}
